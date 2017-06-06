ae53704ce7593243cb3b7b176141fd157ee8f18d
Joshua Summers <joshua.summers@mafuga.com>
2017-06-06 11:44:10 -0700

    Update the build details for the executable to match name for project

--- Console.Client/Properties/AssemblyInfo.cs
+++ Console.Client/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AEO-console-client")]
+[assembly: AssemblyTitle("Console-Client-Tester")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AEO-console-client")]
+[assembly: AssemblyProduct("Console-Client-Tester")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
